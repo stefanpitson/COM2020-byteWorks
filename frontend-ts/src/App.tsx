d3b27d027374b48a645821134bccaa7bcd98293c
andrew.tembra <andrew.tembra@outlook.com>
2026-01-28 13:44:13 +0000

    Added a navbar

--- frontend-ts/src/App.tsx
+++ frontend-ts/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
+import Login2 from './pages/Login2'
 import Home from './pages/Home'
 import './App.css'
 import api from './api'
 import ProtectedRoute from './components/ProtectedRoute'
+import MainLayout from './components/MainLayout'
 
 export default function App() {
   return (
@@ -12,11 +14,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        } />
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<Home />}/>
+        </Route>
+        
+        <Route path="/login2" element={<Login2 />}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend-ts/src/components/MainLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBar';
+import ProtectedRoute from './ProtectedRoute';
+
+export default function MainLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* This stays constant on every page inside this layout */}
+      <NavBar /> 
+
+      {/* The 'Outlet' is where the specific page content (Home, Dashboard, etc.) will appear */}
+      <main className="flex-grow ">
+        <ProtectedRoute>
+            <Outlet />
+        </ProtectedRoute>
+      </main>
+
+      {/* You could also put a Footer here! */}
+    </div>
+  );
+}
