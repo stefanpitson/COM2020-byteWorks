b151a5b5ef93e79b1d6af1a3a651aaba8dc6ccf6
andrew.tembra <andrew.tembra@outlook.com>
2026-01-28 12:42:08 +0000

    Expanded some of the TS class definitons

--- frontend-ts/src/types.ts
+++ frontend-ts/src/types.ts
@@ -1,13 +1,37 @@
 export type Token = string;
 
-export interface User {
-  id?: number;
+export interface Customer {
+  id: number;
   name: string;
   email: string;
-  role?: "Client" | "Business"
+  postCode: string;
+  storeCredit: number;
+  carbonSaved: number
+  rating: number;
 }
 
-export interface Package {
+export interface Seller {
+  id: number
+  name: string;
+  email: string;
+  address: string;
+  phoneNumber: string;
+  carbonSaved: number;
+}
+
+export interface TemplateBundle {
   id: number;
+  estimatedValue: number;
+  cost: number;
+  title: string;
+  description: string;
+  estCarbonSaved: number;
+}
+
+export interface Bundle {
+  id: number;
+  templateID: number;
+  pickedIp: boolean;
+  date: Date;
 }
 
