a97931071bc3aba3f5010c7523b50318736f485a
M-W-BNA <marwbritish@gmail.com>
2026-02-04 14:54:02 +0000

    Expanded testing on auth endpoints

--- tests/test_backend/test_auth.py
+++ tests/test_backend/test_auth.py
@@ -1,6 +1,10 @@
 # THIS SCRIPT MUST BE RUN FROM INSIDE BACKEND FOLDER  -> BECAUSE FUNCTION IMPORTS FROM FILES ONLY WORK FROM BACKEND FOLDER
-# WITH COMMAND python -m pytest -v -s <FULL PATH TO TEST_AUTH.PY>
-# CREATE A .env.test FILE INSIDE backend/ WITH SECRET_KEY and HASH_ALGORITHM parameters for testing
+# WITH COMMAND python -m pytest -v -s <FULL PATH TO TEST_AUTH.PY> -W ignore::DeprecationWarning
+# CREATE A .env.test FILE INSIDE backend/ WITH SECRET_KEY and HASH_ALGORITHM PARAMETERS FOR TESTING
+# CREATE A SECRET_KEY IN THE TERMINAL WITH COMMAND python -c "import secrets; print(secrets.token_urlsafe(32))" 
+# HASH_ALGORITHM=256
+
+# TESTS  FOLLOW Given-When-Then naming convention of tests
 
 from dotenv import load_dotenv
 load_dotenv(".env.test") # loads the test env variables, create this locally with SECRET_KEY and HASH_ALGORITHM to test
@@ -37,11 +41,13 @@ testClient = TestClient(app=app, base_url="https://bytework.online/")
 
 @pytest.fixture(autouse=True)
 def setup_test_db():
-    SQLModel.metadata.create_all(test_engine)
-    yield
-    SQLModel.metadata.drop_all(test_engine)
+    SQLModel.metadata.create_all(test_engine) # setup local test database
+    yield # tests run at this point
+    SQLModel.metadata.drop_all(test_engine) # destroy local test database
 
 
+# FUNCTION FOR CREATING EXISTING USERS IN THE DATABASE DIRECTLY
+# test_user() must then be passed into the test function
 # @pytest.fixture
 # def test_user():
 #     with Session(test_engine) as session:
@@ -55,32 +61,80 @@ def setup_test_db():
 #         session.refresh(test_user)
 #         return test_user
 
-def test_register_success():
-    register_response = testClient.post("/customer/signup", 
+def test_register_login_success():
+    test_email = "test2@exeter.ac.uk"
+    test_password = "password456"
+
+    register_response = testClient.post("/register/customer", 
         json = {
-            "email": "test2@exeter.ac.uk",
-            "password": "password456",
-            "name": "tester",
+            "user": {
+                "email": test_email,
+                "password": test_password,
+                "role": "customer"
+            },
+            "customer": {
+                "name": "tester",
+                "post_code": "ab1 2cd"
+            }
     })
-    assert register_response.status_code == 200
-    print(register_response.text)
 
     login_response = testClient.post("/login", json={
-        "email": "test2@exeter.ac.uk", 
-        "password": "password456"
+        "email": test_email, 
+        "password": test_password
     })
-    
+
+    login_response_data = login_response.json()
+
+
+    assert register_response.status_code == 200
+    assert register_response.text == "{\"message\":\"Customer account created successfully\"}"
+
+    assert login_response_data["user"]["email"] == test_email
+    assert login_response_data["user"]["role"] == "customer"
     assert login_response.status_code == 200
 
 
-
-# def test_login_success():
-#     response = testClient.post("/login", json={
-#         "email": "test@exeter.ac.uk", 
-#         "password": "password456"
-#     })
+def test_login_fail():
+    login_response = testClient.post("/login", json={
+        "email": "test@exeter.ac.uk", 
+        "password": "password456"
+    })
     
-#     assert response.status_code == 200
-#     data = response.json()
-#     print(data)
+    assert login_response.status_code == 401
+    assert login_response.text == "{\"detail\":\"Incorrect email or password\"}"
+
+def test_repeat_register_fail():
+    test_email = "test2@exeter.ac.uk"
+    test_password = "password456"
+
+    register_response = testClient.post("/register/customer", 
+        json = {
+            "user": {
+                "email": test_email,
+                "password": test_password,
+                "role": "customer"
+            },
+            "customer": {
+                "name": "tester",
+                "post_code": "ab1 2cd"
+            }
+    })
+    register_response_2 = testClient.post("/register/customer", 
+        json = {
+            "user": {
+                "email": test_email,
+                "password": test_password,
+                "role": "customer"
+            },
+            "customer": {
+                "name": "tester",
+                "post_code": "ab1 2cd"
+            }
+    })
+
+    assert register_response.status_code == 200
+    assert register_response.text == "{\"message\":\"Customer account created successfully\"}"
+
+    assert register_response_2.status_code == 400
+    assert register_response_2.text == "{\"detail\":\"Email already registered\"}"
 
