1049cbad609b898910fc691530995f581ab372b4
M-W-BNA <marwbritish@gmail.com>
2026-02-08 12:36:42 +0000

    created tests for customer.py endpoint

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,26 +1,31 @@
-def test_get_profile_success(test_client):
-    # Retrieving customer profile using a valid token
-    test_email = "test2@exeter.ac.uk"
-    test_password = "password456"
-    test_name = "tester"
-    test_post_code = "ab1 2cd"
-
-    register_response = test_client.post("/register/customer", 
-        json = {
-            "user": {
-                "email": test_email,
-                "password": test_password,
-                "role": "customer"
-            },
-            "customer": {
-                "name": test_name,
-                "post_code": test_post_code
-            }
+def test_get_profile_success(test_client, registered_customer):
+    customer = registered_customer["customer_data"]
+    login_response = test_client.post("/login", json={
+        "email": customer["user"]["email"], 
+        "password": customer["user"]["password"]
     })
 
+    login_response_data = login_response.json()
+    token = login_response_data["access_token"]
+    profile_response = test_client.get(
+        "/customers/profile",
+        headers={"Authorization": "Bearer " + token}
+    )
+    profile_response_data = profile_response.json()
+    profile_name = profile_response_data["name"]
+    profile_post_code = profile_response_data["post_code"]
+    profile_customer_id = profile_response_data["customer_id"]
+
+    assert profile_response.status_code == 200
+    assert customer["customer"]["name"] == profile_name
+    assert customer["customer"]["post_code"] == profile_post_code
+    assert profile_customer_id > 0
+
+def test_get_profile_wrong_role_fail(test_client ,registered_vendor):
+    vendor = registered_vendor["vendor_data"]
     login_response = test_client.post("/login", json={
-        "email": test_email, 
-        "password": test_password
+        "email": vendor["user"]["email"], 
+        "password": vendor["user"]["password"]
     })
 
     login_response_data = login_response.json()
@@ -31,13 +36,13 @@ def test_get_profile_success(test_client):
         headers={"Authorization": "Bearer " + token}
     )
 
-    profile_response_data = profile_response.json()
-    profile_name = profile_response_data["name"]
-    profile_post_code = profile_response_data["post_code"]
-    profile_customer_id = profile_response_data["customer_id"]
+    assert profile_response.text == "{\"detail\":\"Not a customer account\"}"
+    assert profile_response.status_code == 403
 
-    assert profile_response.status_code == 200
-    assert test_name == profile_name
-    assert test_post_code == profile_post_code
-    assert profile_customer_id > 0
+def test_get_profile_with_no_account_fail(test_client, registered_customer):
+    '''Login, recieve JWT token, delete account (if that functiontionality is added), then attempt get profile'''
 
+    # ...
+
+    # assert profile_response.text == "{\"detail\":\"Profile not found\"}"
+    # assert profile_response.status_code == 404
