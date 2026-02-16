33ad7d3ce25bfbd7ed1977381868720c814bf6bb
stefanpitson <stefanpitson@gmail.com>
2026-02-16 21:23:05 +0000

    added reservation link to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -22,6 +22,12 @@ const UserIcon = () => (
   </svg>
 );
 
+const BagIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 mr-2">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+  </svg>
+);
+
 export default function NavBar() {
   const navigate = useNavigate();
   const [isSettingsOpen, setSettingsOpen] = useState<boolean>(false);
@@ -126,6 +132,15 @@ export default function NavBar() {
                   <SettingsIcon />
                   Settings
                 </button>
+              </li>
+              <li>
+                <Link
+                  to="/customer/reservations"
+                  className="w-full text-left px-4 py-2.5 hover:bg-gray-50 hover:text-[hsl(158,48%,46%)] flex items-center transition-colors"
+                >
+                  <BagIcon />
+                  Reservations
+                </Link>
               </li>
               <li>
                 <button 
