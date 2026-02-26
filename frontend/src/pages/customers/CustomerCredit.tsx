98eac2661a10a2d68979a60ad0e98f13f0185475
stefanpitson <stefanpitson@gmail.com>
2026-02-26 21:08:30 +0000

    turned credit display into a clickable link to the credit top up page

---
+++ frontend/src/pages/customers/CustomerCredit.tsx
@@ -0,0 +1,7 @@
+export default function CustomerCredit() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-background">
+      <h1> Customer Credit Page</h1>
+    </div>
+  );
+}
