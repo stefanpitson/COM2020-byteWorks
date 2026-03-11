8f507e29b726dc095184a5a221fd0e8cb3a886b0
andrew.tembra <andrew.tembra@outlook.com>
2026-03-11 21:03:51 +0000

    Added and fixed endpoints

--- backend/app/schema.py
+++ backend/app/schema.py
@@ -74,6 +74,12 @@ class VendorSignupRequest(BaseModel):
         phone_number: str
         opening_hours: str
 
+class PasswordCheck(BaseModel):
+    password: str
+
+class PasswordCheckRead(BaseModel):
+    valid: bool
+
 #data for updating the customer 
 # all are optional as only updated information is given
 class CustomerUpdate(BaseModel):
